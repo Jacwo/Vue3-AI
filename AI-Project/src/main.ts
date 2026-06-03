8e8a2e3f731aa64dca6859bdc728b8e179d4526b
yangyuanliang <yangyuanliang@wisepool.com.cn>
2026-06-03 11:59:32 +0800

    更新

--- AI-Project/src/main.ts
+++ AI-Project/src/main.ts
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import './assets/main.css'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
+import VConsole from 'vconsole'
 
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+
+// 移动端调试工具
+new VConsole()
 
 const app = createApp(App)
 const pinia = createPinia()
